d3e964d8caaf6de7b15fc0144fa56a47acfbdf49
doia7002 <doia7002@naver.com>
2023-12-19 14:01:22 +0900

    [feat] Add Tree2,3
    나무 2,3 추가

--- Assets/02.Scripts/Environments/Resource.cs
+++ Assets/02.Scripts/Environments/Resource.cs
@@ -8,15 +8,15 @@ public class Resource : MonoBehaviour
     public int quantityPerHit = 1;
     public int capacity;
 
-    public int Gather(Vector3 hitPoint, Vector3 hitNormal)
+    public void Gather(Vector3 hitPoint, Vector3 hitNormal)
     {
         for (int i = 0; i < quantityPerHit; i++)
         {
-            if (capacity <= 0)
-            { break; }
+            if (capacity <= 0) { break; }
             capacity -= 1;
             Instantiate(itemToGive.dropPrefab, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up));
         }
+
         if (capacity <= 0)
             Destroy(gameObject);
     }
