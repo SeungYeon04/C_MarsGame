b0dc23efc2e2e866689865dce6b6a45164dc287f
simderella <146172523+simderella@users.noreply.github.com>
2023-12-20 21:17:41 +0900

    [feat] Fifth Merge

--- Assets/02.Scripts/Equip/EquipTool.cs
+++ Assets/02.Scripts/Equip/EquipTool.cs
@@ -45,4 +45,23 @@ public class EquipTool : Equip
     {
         attacking = false;
     }
+    public void OnHit()
+    {
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, attackDistance))
+        {
+            if (doesGatherResources && hit.collider.TryGetComponent(out Resource resouce))
+            {
+                resouce.Gather(hit.point, hit.normal);
+            }
+
+            if (doesDealDamage && hit.collider.TryGetComponent(out IDamagable damageable))
+            {
+                damageable.TakePhysicalDamage(damage);
+            }
+        }
+
+    }
 }
